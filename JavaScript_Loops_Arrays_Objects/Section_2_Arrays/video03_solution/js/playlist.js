079390f003435a449c12c54b5a058ca7a2fce28e
Bill Shannon <bill@billshannon.com>
2015-09-12 22:32:22 -0600

    revise quiz to display correct and incrrect questions

--- JavaScript_Loops_Arrays_Objects/Section_2_Arrays/video03_solution/js/playlist.js
+++ JavaScript_Loops_Arrays_Objects/Section_2_Arrays/video03_solution/js/playlist.js
@@ -3,4 +3,4 @@ playList.push('I Did It My Way');
 playList.push('Respect', 'Imagine');
 playList.unshift('Born to Run');
 playList.unshift('Louie Louie', 'Maybellene');
-printList( playList );
+printSongs( playList );
--- quizArray/js/quiz.js
+++ quizArray/js/quiz.js
@@ -1,18 +1,46 @@
 function print(message) {
-  document.write(message);
+    var outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = message;
+}
+
+function buildList(arr) {
+    var listHTML = '<ol>';
+    for (var i = 0; i < arr.length; i++) {
+        listHTML += '<li>' + arr[i] + '</li>';
+    }
+    listHTML += '</ol>';
+    return listHTML
 }
 
 var questions = [['How many hours are in a day?', '24'], ['Who is the president?', 'Barack Obama'], ['What is the largest state in the United States?', 'Alaska']];
 
 var totalCorrect = 0;
+var right = [];
+var wrong = [];
+var html;
+var correctQuestions;
+var incorrectQuestions;
+
 for (var i = 0; i < questions.length; i++) {
-     var ask = prompt(questions[i][0]);
-     var answer = questions[i][1];
-     if ( ask === answer) {
-         totalCorrect += 1
-     } else {
-         totalCorrect
-     }
+    var ask = prompt(questions[i][0]);
+    var answer = questions[i][1];
+    if (ask === answer) {
+        totalCorrect += 1
+        right.push(questions[i][0])
+    }
+    else {
+        wrong.push(questions[i][0])
+    }
 }
-var correctResponses = "You got " + totalCorrect + " correct!"
-prompt(correctResponses)
+
+for (var i = 0; i < right.length; i++) {
+    correctQuestions = "These are the questions you got correct:<br><br> " + right[i][0];
+    incorrectQuestions = "These are the questions you got wrong:<br><br> " + wrong[i][0];
+}
+
+html = "You got " + totalCorrect + " question(s) right.";
+html += '<h2>You got these questions correct:</h2>';
+html += buildList(right)
+html += '<h2>You got these questions wrong:</h2>';
+html += buildList(wrong)
+print(html)
