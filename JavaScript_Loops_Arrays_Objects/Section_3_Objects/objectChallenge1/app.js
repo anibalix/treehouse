0058c4c9fd27d3b0aba8ac3e16395f602cff3b83
Bill Shannon <bill@billshannon.com>
2015-09-26 13:46:16 -0600

    complete section3

---
+++ JavaScript_Loops_Arrays_Objects/Section_3_Objects/objectChallenge1/app.js
@@ -0,0 +1,32 @@
+var students = [
+    {
+        name: 'Jenny Jones',
+        track: 'Ruby',
+        achievements: 3,
+        points: 300
+    },
+    {
+        name: 'Tom Thomas',
+        track: 'JavaScript',
+        achievements: 2,
+        points: 200
+    },
+    {
+        name: 'Emily Edmonton',
+        track: 'Java',
+        achievements: 1,
+        points: 100
+    },
+    {
+        name: 'Mike Matthew',
+        track: 'Front-end',
+        achievements: 5,
+        points: 500
+    },
+    {
+        name: 'Sarah Smith',
+        track: 'Python',
+        achievements: 8,
+        points: 800
+    }
+]
